6c04e394ea023d2c5ae6a583ee74ee7fa59845dc
Sh-x <39991480+Sh-x@users.noreply.github.com>
2019-08-19 20:38:11 +0800

    damned cool

---
+++ starry.js
@@ -0,0 +1,46 @@
+function Star() {
+    this.x = random(-width, width);
+    this.y = random(-height, height);
+    this.z = random(width);
+
+    this.update = function() {
+        this.z = this.z - 10;
+        if(this.z < 1) {
+            this.z = width;
+            this.x = random(-width, width);
+            this.y = random(-height, height);
+        }
+    }
+
+    this.show = function() {
+        fill(255);
+        noStroke();
+
+        var relativeX = map(this.x/this.z, 0, 1, 0, width);
+        var relativeY = map(this.y/this.z, 0 ,1, 0, height);
+        var radius = map(this.z, 0, width, 20, 0);
+
+        ellipse(relativeX, relativeY, radius, radius);
+    }
+
+}
+
+
+var stars = [];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    for(var i = 0; i < 1000; i++) {
+        stars[i] = new Star();
+    }
+}
+
+function draw() {
+    
+    background(0);
+    translate(width/2, height/2);
+    for(var i = 0; i < stars.length; i++) {
+        stars[i].update();
+        stars[i].show();
+      }
+}
